aff2121627279cd78394f60a0e83fba064c959d3
Rus <rus.zakirov@triosstudent.com>
2024-10-03 09:31:09 -0400

    Added links in index.php to header.php and footer.php files

--- product_manager/index.php
+++ product_manager/index.php
@@ -3,9 +3,13 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Product Page</title>
+   <title>Product Manager - Home</title>
 </head>
 <body>
-   
+   <?php include("header.php");?>
+   <main>
+
+   </main>
+   <?php include("footer.php");?>
 </body>
 </html>
