443d29dff3e32db2d01c18692f6b113d903e3087
Rus <rus.zakirov@triosstudent.com>
2024-10-03 10:05:12 -0400

    Added some styles to main.css and also added a link tag to main.css in index.php

--- product_manager/index.php
+++ product_manager/index.php
@@ -4,6 +4,7 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Product Manager - Home</title>
+   <link rel="stylesheet" href="/Rus_Capstone_Project/product_manager/css/main.css">
 </head>
 <body>
    <?php include("header.php");?>
