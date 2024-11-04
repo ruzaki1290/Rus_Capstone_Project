510ac0486993071d1b21801b64240e610ebe9d4e
Rus <rus.zakirov@triosstudent.com>
2024-11-04 09:03:16 -0500

    Edited checkout.php page to include styles for a checkout button

--- product_manager/checkout/checkout.php
+++ product_manager/checkout/checkout.php
@@ -16,6 +16,22 @@
    <?php include("../view/header.php");?>
    <main>
       <h2>Checkout</h2>
+      <form action="process_order.php" method="post">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required><br>
+
+          <label for="address">Address:</label>
+          <input type="text" id="address" name="address" required><br>
+
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required><br>
+
+          <label for="phone">Phone:</label>
+          <input type="tel" id="phone" name="phone" required><br>
+
+          <input type="submit" value="Submit Order" class="submit-button">
+      </form>
+      <p><a href="../cart/cart.php">Go Back</a></p>
    </main>
 </body>
    <?php include("../view/footer.php");?>
